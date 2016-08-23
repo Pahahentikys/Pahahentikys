9e57713aad18a79c6338478eff608b20e1ff2e98
Pavel <Pavel@DESKTOP-DVD37LC>
2016-08-23 17:13:44 +0300

    add comment in homeContoller

---
+++ ClientForKOMK/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientForKOMK.MvcApplication" Language="C#" %>
---
+++ ClientForKOMK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClientForKOMK.Startup))]
+namespace ClientForKOMK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
